d015bdc93c03136c4a69045cc9aec8220e57aee7
Yewei0302 <zacztb@gmail.com>
2026-09-03 01:09:27 +0800

    更换个人头像并关闭看板娘

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,18 +2,18 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "叶工",
-	bio: "自动化工程师｜记录PLC、电气工程与自动化项目",
-	typewriter: {
-		enable: true, // 启用个人简介打字机效果
-		speed: 80, // 打字速度（毫秒）
-	},
-	links: [
-		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/Yewei0302",
-		},
-	],
+  avatar: "/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+  name: "叶工",
+  bio: "自动化工程师｜记录PLC、电气工程与自动化项目",
+  typewriter: {
+    enable: true, // 启用个人简介打字机效果
+    speed: 80, // 打字速度（毫秒）
+  },
+  links: [
+    {
+      name: "GitHub",
+      icon: "fa7-brands:github",
+      url: "https://github.com/Yewei0302",
+    },
+  ],
 };
