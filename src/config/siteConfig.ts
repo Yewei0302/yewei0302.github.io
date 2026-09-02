bff7976aa1ad8865bdbcf57f112beae34eb31c2c
Yewei0302 <zacztb@gmail.com>
2026-09-02 23:27:40 +0800

    Update siteConfig.ts
    更新博客名称和站点信息

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,13 +1,13 @@
 import type { SiteConfig } from "../types/config";
 
 // 定义站点语言
-const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
-	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	title: "叶工的自动化学习笔记",
+	subtitle: "记录PLC、电气工程与自动化项目的学习与实践",
+	siteURL: "https://yewei0302.github.io/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2026-09-02", // 站点开始运行日期，用于站点统计组件计算运行天数
 	timeZone: "Asia/Shanghai", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
 
 	lang: SITE_LANG,
